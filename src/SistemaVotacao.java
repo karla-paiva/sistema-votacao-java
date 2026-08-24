bcd2297f7d111ab7b142d97392f74209b36a07f5
Karla Victoria Andrade Paiva <karla.11780586@aluno.mg.gov.br>
2026-08-24 13:51:32 -0300

    feat: definir limites de candidatos turmas e votantes

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -8,4 +8,7 @@ public class SistemaVotacao {
         System.out.println("Sistema de votação iniciado.");
         scanner.close();
     }
+    static final int MAX_CANDIDATOS = 5;
+    static final int TOTAL_TURMAS = 3;
+    static final int MAX_VOTANTES_POR_TURMA = 10;
 }
